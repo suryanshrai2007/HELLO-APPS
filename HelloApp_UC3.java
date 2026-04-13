2885ef11a4153822b566a35dd9f730b6afb52052
suryanshrai2007 <suryanshrai2007@gmail.com>
2026-04-13 23:34:32 +0530

    Add files via upload

---
+++ HelloApp_UC3.java
@@ -0,0 +1,11 @@
+public class HelloApp {
+
+    public static void main(String[] args) {
+		String name = "Worlds";
+		if (args.length > 0) {
+			name = args[0];
+		}
+        System.out.println("Hello, " + name + "!");
+    }
+
+}
